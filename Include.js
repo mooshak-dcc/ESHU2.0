0f36da9f3059af9999f1c946855d3171e055cdc0
josepaiva94 <josepaiva94@gmail.com>
2023-03-27 20:39:19 +0100

    first commit

---
+++ Include.js
@@ -0,0 +1,67 @@
+document.write('<script src="src/pt/up/fc/dcc/eshu/client/eshugwt/scripts/Eshu/Language.js"><\/script>');
+document.write('<script src="src/pt/up/fc/dcc/eshu/client/eshugwt/scripts/Eshu/Init.js"><\/script>');
+document.write('<script src="src/pt/up/fc/dcc/eshu/client/eshugwt/scripts/Eshu/Eshu.js"><\/script>');
+document.write('<script src="src/pt/up/fc/dcc/eshu/client/eshugwt/scripts/Graph/Graph.js"><\/script>');
+document.write('<script src="src/pt/up/fc/dcc/eshu/client/eshugwt/scripts/Eshu/Events.js"><\/script>');
+document.write('<script src="src/pt/up/fc/dcc/eshu/client/eshugwt/scripts/Eshu/Layout.js"><\/script>');
+document.write('<script src="src/pt/up/fc/dcc/eshu/client/eshugwt/scripts/Eshu/NodeType.js"><\/script>');
+document.write('<script src="src/pt/up/fc/dcc/eshu/client/eshugwt/scripts/Eshu/EdgeType.js"><\/script>');
+document.write('<script src="src/pt/up/fc/dcc/eshu/client/eshugwt/scripts/Eshu/ContextMenu.js"><\/script>');
+document.write('<script src="src/pt/up/fc/dcc/eshu/client/eshugwt/scripts/Eshu/FormatPanel.js"><\/script>');
+document.write('<script src="src/pt/up/fc/dcc/eshu/client/eshugwt/scripts/Eshu/Map.js"><\/script>');
+document.write('<script src="src/pt/up/fc/dcc/eshu/client/eshugwt/scripts/Eshu/Configuration.js"><\/script>');
+document.write('<script src="src/pt/up/fc/dcc/eshu/client/eshugwt/scripts/Eshu/AlgorithmDispersal.js"><\/script>');
+document.write('<script src="src/pt/up/fc/dcc/eshu/client/eshugwt/scripts/Graph/Vertices.js"><\/script>');
+document.write('<script src="src/pt/up/fc/dcc/eshu/client/eshugwt/scripts/Graph/Box.js"><\/script>');
+document.write('<script src="src/pt/up/fc/dcc/eshu/client/eshugwt/scripts/Graph/Edge.js"><\/script>');
+document.write('<script src="src/pt/up/fc/dcc/eshu/client/eshugwt/scripts/Graph/RectangleSelection.js"><\/script>');
+//document.write('<script src="scripts/UseCase/UseCase.js"><\/script>');
+//document.write('<script src="scripts/UseCase/Actor.js"><\/script>');
+//document.write('<script src="scripts/UseCase/Association.js"><\/script>');
+//document.write('<script src="scripts/UseCase/Note.js"><\/script>');
+//document.write('<script src="scripts/UseCase/Note.js"><\/script>');
+document.write('<script src="src/pt/up/fc/dcc/eshu/client/eshugwt/scripts/Eshu/XMLParser.js"><\/script>');
+document.write('<script src="src/pt/up/fc/dcc/eshu/client/eshugwt/scripts/Eshu/Tollbar.js"><\/script>');
+document.write('<script src="src/pt/up/fc/dcc/eshu/client/eshugwt/scripts/Graph/QuadTree.js"><\/script>');
+document.write('<script src="src/pt/up/fc/dcc/eshu/client/eshugwt/scripts/Graph/TextBox.js"><\/script>');
+document.write('<script src="src/pt/up/fc/dcc/eshu/client/eshugwt/scripts/Graph/Rectangle.js"><\/script>');
+document.write('<script src="src/pt/up/fc/dcc/eshu/client/eshugwt/scripts/Graph/ContainerTextbox.js"><\/script>');
+document.write('<script src="src/pt/up/fc/dcc/eshu/client/eshugwt/scripts/Graph/ComplexVertice.js"><\/script>');
+document.write('<script src="src/pt/up/fc/dcc/eshu/client/eshugwt/scripts/Commands/Command.js"><\/script>');
+document.write('<script src="src/pt/up/fc/dcc/eshu/client/eshugwt/scripts/Commands/CommandStack.js"><\/script>');
+document.write('<script src="src/pt/up/fc/dcc/eshu/client/eshugwt/scripts/Commands/Insert.js"><\/script>');
+document.write('<script src="src/pt/up/fc/dcc/eshu/client/eshugwt/scripts/Commands/Remove.js"><\/script>');
+document.write('<script src="src/pt/up/fc/dcc/eshu/client/eshugwt/scripts/Commands/Move.js"><\/script>');
+document.write('<script src="src/pt/up/fc/dcc/eshu/client/eshugwt/scripts/Commands/Paste.js"><\/script>');
+document.write('<script src="src/pt/up/fc/dcc/eshu/client/eshugwt/scripts/Commands/Resize.js"><\/script>');
+document.write('<script src="src/pt/up/fc/dcc/eshu/client/eshugwt/scripts/Commands/ChangeType.js"><\/script>');
+document.write('<script src="src/pt/up/fc/dcc/eshu/client/eshugwt/scripts/Eshu/ImporExportJSON.js"><\/script>'); 
+
+
+
+
+
+/*document.write('<script src="Nodes/Vertices.js"><\/script>');
+document.write('<script src="Edges/Edge.js"><\/script>');
+document.write('<script src="Edges/EEREdge1.js"><\/script>');
+document.write('<script src="Edges/EEREdge2.js"><\/script>');
+document.write('<script src="Graph/Language.js"><\/script>');
+document.write('<script src="Graph/Graph.js"><\/script>');
+document.write('<script src="Graph/Init.js"><\/script>');
+document.write('<script src="Graph/Option.js"><\/script>');
+document.write('<script src="Nodes/Attribute.js"><\/script>');
+document.write('<script src="Nodes/EspGenCat.js"><\/script>');
+document.write('<script src="Nodes/Entity.js"><\/script>');
+document.write('<script src="Nodes/Relationship.js"><\/script>');
+document.write('<script src="Nodes/SimpleNode.js"><\/script>');
+document.write('<script src="Graph/Box.js"><\/script>');
+document.write('<script src="Graph/CreateContextMenu.js"><\/script>');
+document.write('<script src="Graph/Editor.js"><\/script>');
+document.write('<script src="Graph/ConversionGraphJson.js"><\/script>');
+document.write('<script src="Edges/Link.js"><\/script>'); */
+
+
+
+
+
+
